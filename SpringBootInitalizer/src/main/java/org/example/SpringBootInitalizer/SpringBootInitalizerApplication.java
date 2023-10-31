4dcd24fe67fbb2e3eb177e3c6a33e51d1efb48d0
Michael Anchundia <michaelanchundia@Michaels-MacBook-Air.local>
2023-10-30 20:17:57 -0400

    Put spring boot project here

---
+++ SpringBootInitalizer/src/main/java/org/example/SpringBootInitalizer/SpringBootInitalizerApplication.java
@@ -0,0 +1,13 @@
+package org.example.SpringBootInitalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitalizerApplication.class, args);
+	}
+
+}
